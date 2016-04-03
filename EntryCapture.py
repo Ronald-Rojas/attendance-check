23268c8340d1615222c861d8b435d7f12c4f8752
rojasreinold <rojasreinold@gmail.com>
2016-04-03 14:20:22 +0000

    Entry almost done

--- EntryCapture.py
+++ EntryCapture.py
@@ -1,6 +1,27 @@
 import RPi.GPIO as GPIO
 import time
+import datetime
+from subprocess import call
 
+classinformation = [[0 for i in xrange(4)] for i in xrange(5)]
+classinformation[0][0] = "cs404"
+classinformation[0][1] = datetime.date(2016,4,3)
+classinformation[0][2] = datetime.time(12,30)
+classinformation[0][3] = datetime.time(12,30)
+def checkDistance():
+    GPIO.output(TRIG,1)
+    time.sleep(0.00001)
+    GPIO.output(TRIG,0)
+
+    while GPIO.input(ECHO) ==0:
+        pass
+    start = time.time()
+
+    while GPIO.input(ECHO) == 1:
+        pass
+    stop = time.time()
+
+    return (stop - start) * 170
 
 
 GPIO.setmode(GPIO.BOARD)
@@ -19,17 +40,24 @@ GPIO.output(TRIG,1)
 time.sleep(0.00001)
 GPIO.output(TRIG,0)
 
-while GPIO.input(ECHO) ==0:
-    pass
-start = time.time()
 
-while GPIO.input(ECHO) == 1:
-    pass
-stop = time.time()
-
-doorDistance = (stop - start) * 170
+doorDistance = checkDistance()
 
 print "Setup has finished"
 print doorDistance
+
+
+classID = (raw_input("what is your classID for today?"))
+classIndex = 0
+while classID != classinformation[classIndex][0]:
+    classIndex= classIndex +1
+    print "adding to class index"
+currentTime = datetime.time(datetime.datetime.now().hour,datetime.datetime.now().minute)
+
+while currentTime < classinformation[classIndex][3]:
+    time.sleep(0.1)
+    if checkDistance > doorDistance +.5 or checkDistance < doorDistance +.5:
+        call(["raspistill -ss .1 -o image.jpeg"])
+
 
 GPIO.cleanup()
