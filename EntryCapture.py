c0aabf6e99661748377e8d50d38fa5721b9aff58
rojasereinold <rojasreinold@gmail.com>
2016-04-04 16:55:51 -0400

    adding some clarifai information

--- EntryCapture.py
+++ EntryCapture.py
@@ -2,6 +2,12 @@ import RPi.GPIO as GPIO
 import time
 import datetime
 from subprocess import call
+#The Following is the start up of the program
+
+curl -x POST "https://api.clarifai.com/v1/token/" \
+        -d "client_id={client_id}" \
+        -d "client_secret={client_secret}"\
+        -d "grant_type=client_credentials"
 
 classinformation = [[0 for i in xrange(4)] for i in xrange(5)]
 classinformation[0][0] = "cs404"
